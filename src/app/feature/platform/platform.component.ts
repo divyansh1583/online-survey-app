65b17e3cb16cbff5acca736215843084361e4851
Divyansh Chauhan <106877188+divyansh1583@users.noreply.github.com>
2024-11-24 01:12:35 +0530

    changes

--- src/app/feature/platform/platform.component.ts
+++ src/app/feature/platform/platform.component.ts
@@ -1,20 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
-import { MatSidenavModule } from '@angular/material/sidenav';
 import { SidebarComponent } from '../../shared/layout/sidebar/sidebar.component';
-import { Sidebar2Component } from "../../shared/layout/sidebar2/sidebar2.component";
+import { HeaderComponent } from "../../shared/layout/header/header.component";
 
 @Component({
   selector: 'app-platform',
   standalone: true,
   imports: [
     CommonModule,
-    Sidebar2Component
+    HeaderComponent,
+    SidebarComponent
 ],
   templateUrl: './platform.component.html',
   styleUrl: './platform.component.scss'
 })
 export class PlatformComponent {
+  @ViewChild(SidebarComponent)
+  sidebar!: SidebarComponent;
+
+  toggleSidebar(): void {
+    this.sidebar.toggleSidebar();
+  }
 
 }
